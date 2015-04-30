a55732f57de012fd2e67228be0bdfcc98e2f7da1
沈红榜 <shenhongbang@appletekiiMac.local>
2015-04-30 18:32:59 +0800

    no message

--- TheTestDemo/SearchViewController.m
+++ TheTestDemo/SearchViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SearchViewController.h"
+#import "CollViewController.h"
 
 @interface SearchViewController ()
 
@@ -17,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRedo target:self action:@selector(tempAction)];
     
 //    UIBarButtonItem *leftItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRedo target:self action:@selector(goBack)];
 //    self.navigationItem.leftBarButtonItem = leftItem;
@@ -34,19 +37,29 @@
 //    self.navigationItem.titleView = searchBar;
     
 
-    SHBSearchBar *shbBar = [[SHBSearchBar alloc] initWithFrame:CGRectMake(0, 0, 100, 30)];
-    shbBar.borderStyle = UITextBorderStyleRoundedRect;
-    shbBar.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"你喜欢的话题 (不要超过4个字)" attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:11], NSForegroundColorAttributeName : [UIColor redColor]}];
-    UIImageView *leftView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
-    leftView.image = [UIImage imageNamed:@"account-circle-normal"];
-    shbBar.leftView = leftView;
-    shbBar.leftViewMode = UITextFieldViewModeUnlessEditing;
-    self.navigationItem.titleView = shbBar;
+//    SHBSearchBar *shbBar = [[SHBSearchBar alloc] initWithFrame:CGRectMake(0, 0, 323, 30)];
+//    shbBar.borderStyle = UITextBorderStyleRoundedRect;
+//    shbBar.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"你喜欢的话题 (不要超过4个字)" attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:13], NSForegroundColorAttributeName : [UIColor redColor]}];
+//    UIImageView *leftView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 10, 15)];
+//    leftView.image = [UIImage imageNamed:@"Rectangle 462"];
+//    shbBar.leftView = leftView;
+//    shbBar.leftViewMode = UITextFieldViewModeUnlessEditing;
+//    
+//    self.navigationItem.titleView = shbBar;
     
+    CollViewController *collVC = [[CollViewController alloc] init];
     
+    UISearchController *searchController = [[UISearchController alloc] initWithSearchResultsController:collVC];
+    
+    self.navigationItem.titleView = searchController.searchBar;
+    
+    self.view.backgroundColor = [UIColor grayColor];
     
 }
 
+- (void)tempAction {
+    
+}
 
 
 - (CGRect)placeholderRectForBounds:(CGRect)bounds {
@@ -75,20 +88,29 @@
 @end
 
 
-@implementation SHBSearchBar
+@implementation SHBSearchBar {
+    CGRect _rect;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        
+        _rect = frame;
     }
     return self;
 }
 
-- (void)drawPlaceholderInRect:(CGRect)rect {
-    CGSize size = [@"请" sizeWithAttributes:@{NSFontAttributeName : [UIFont systemFontOfSize:11]}];
+- (void)drawRect:(CGRect)rect {
+    [super drawRect:rect];
     
+}
+
+
+- (void)drawPlaceholderInRect:(CGRect)rect {
+    CGSize size = [@"请" sizeWithAttributes:@{NSFontAttributeName : [UIFont systemFontOfSize:13]}];
+    
+    rect = _rect;
     CGRect kkRect = [self.attributedPlaceholder boundingRectWithSize:CGSizeMake(0, size.height) options:NSStringDrawingUsesFontLeading | NSStringDrawingUsesLineFragmentOrigin context:nil];
     [self.attributedPlaceholder drawInRect:CGRectMake(rect.origin.x, CGRectGetMaxY(rect) - CGRectGetHeight(rect) / 2. - size.height / 2., kkRect.size.width, kkRect.size.height)];
 }
